d18a7b65416acc9f7a4f84cec9b91d246b6964f6
valonso <valonsodev@gmail.com>
2023-10-14 17:34:25 +0200

    The bug fixed itself

--- src/inline.ts
+++ src/inline.ts
@@ -5,10 +5,15 @@ execSync("bun run build");
 const script = await Bun.file("dist/index.js").text();
 const css = await Bun.file("dist/index.css").text();
 
+
 let html = await Bun.file("index.html").text();
 
-html = html.replace(`<script src="dist/index.js" type="module"></script>`, `<script type="module">${script}</script>`);
+html = html.replace(
+    `<script src="dist/index.js" type="module"></script>`,
+    `<script type="module">\n\n${script}\n\n</script>`
+);
 
+// await Bun.write("dist/index_inter.js", `<script type="module">\n\n${script}\n\n</script>`);
 html = html.replace(`<link href="dist/index.css" rel="stylesheet">`, `<style>${css}</style>`);
 
 await Bun.write("dist/index.html", html);
